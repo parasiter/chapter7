31d1797ddb0370cbe57248dd7f83ffe756b3c32d
parasiter <937247098@qq.com>
2017-11-01 20:39:40 +0800

    111

---
+++ Chapter7/Assets/C#scipt/OrbitCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour {
+	[SerializeField] private Transform target;
+	public float _rotY;
+	private Vector3 _offset;
+	public float rotSpeed = 1.5f;
+	// Use this for initialization
+	void Start () {
+		_rotY = transform.eulerAngles.y;
+		_offset = target.position - transform.position;
+	}
+	
+	// Update is called once per framev
+	void Update(){
+	}
+
+	void LateUpdate () {
+		float horInput = Input.GetAxis ("Horizontal");
+		if (horInput != 0) {
+			_rotY += horInput * rotSpeed;
+		} else {
+			_rotY += Input.GetAxis ("Mouse X") * rotSpeed * 3;
+		}
+		Quaternion rotation = Quaternion.Euler (0, _rotY, 0);
+		transform.position = target.position - rotation * _offset;
+		transform.LookAt (target);
+	}
+}
---
+++ Chapter7/Assets/C#scipt/RelativeMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelativeMovement : MonoBehaviour {
+	[SerializeField] private Transform target;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 movement = Vector3.zero;
+		float horInput = Input.GetAxis ("horizontal");
+		float vertInput = Input.GetAxis ("Vertical");
+		if(horInput!=0||vertInput!=0){
+			movement.x = horInput;
+			movement.z = vertInput;
+
+			Quaternion temp = target.rotation;
+			target.eulerAngles = new Vector3 (0, target.eulerAngles.y, 0);
+			movement = target.TransformDirection (movement);
+			target.rotation = temp;
+
+			transform.rotation = Quaternion.LookRotation (movement);
+	}
+}
